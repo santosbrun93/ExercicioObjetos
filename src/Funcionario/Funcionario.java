21d4516b1e9709e1165c73401f1628f7d45d42d8
Bruno Silva <santos.brun93@gmail.com>
2022-02-15 21:52:52 -0300

    Exercio 1 finalizado

---
+++ src/Funcionario/Funcionario.java
@@ -0,0 +1,103 @@
+package Funcionario;
+
+import java.sql.Time;
+
+public class Funcionario {
+
+    String nome;
+
+    String sobrenome;
+
+    String cidade;
+
+    String estado;
+
+    int horasTrabalhadas;
+
+    double valorHora;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public int getHorasTrabalhadas() {
+        return horasTrabalhadas;
+    }
+
+    public void setHorasTrabalhadas(int horasTrabalhadas) {
+        this.horasTrabalhadas = horasTrabalhadas;
+    }
+
+    public double getValorHora() {
+        return valorHora;
+    }
+
+    public void setValorHora(double valorHora) {
+        this.valorHora = valorHora;
+    }
+
+    public Funcionario() {
+    }
+
+
+    @Override
+    public String toString() {
+        return "Funcionario{" +
+                "nome='" + nome + '\'' +
+                ", sobrenome='" + sobrenome + '\'' +
+                '}';
+    }
+
+    public Funcionario(String nome, String sobrenome, String cidade, String estado, int horasTrabalhadas, double valorHora) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.horasTrabalhadas = horasTrabalhadas;
+        this.valorHora = valorHora;
+    }
+
+    public void incrementarHoras(int horas, Funcionario funcionario){
+        horas += funcionario.getHorasTrabalhadas();
+        funcionario.setHorasTrabalhadas(horas);
+    }
+
+
+    public double calcularSalario (Funcionario funcionario){
+        double valorSalario = funcionario.getHorasTrabalhadas() * funcionario.getValorHora();
+
+        return valorSalario;
+    }
+
+    public void alterarValorHora( Funcionario funcionario, double valorNovo){
+        funcionario.setValorHora(valorNovo);
+    }
+}
---
+++ src/Funcionario/Main.java
@@ -0,0 +1,83 @@
+package Funcionario;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        String nome;
+
+        String sobrenome;
+
+        String cidade;
+
+        String estado;
+
+        int horasTrabalhadas;
+
+        double valorHora;
+
+
+        System.out.println("Insira o nome do funcionário: ");
+        nome = entrada.nextLine();
+
+        System.out.println("Insira o sobrenome do funcionário: ");
+        sobrenome = entrada.nextLine();
+
+        System.out.println("Insira a Cidade do funcionário: ");
+        cidade = entrada.nextLine();
+
+        System.out.println("Insira o Estado do funcionário: ");
+        estado = entrada.nextLine();
+
+        System.out.println("Insira a quantidade de horas do funcionário: ");
+        horasTrabalhadas = entrada.nextInt();
+
+        System.out.println("Insira o valor por hora do funcionário: ");
+        valorHora = entrada.nextDouble();
+
+        Funcionario numero1 = new Funcionario(nome, sobrenome, cidade, estado, horasTrabalhadas, valorHora);
+
+        int opcao = 0;
+
+        do {
+
+
+
+            System.out.println("1 - Incrementar horas trabalhadas no mês: ");
+            System.out.println("2 - Alterar valor hora: ");
+            System.out.println("3 - Calcular valor do salário mensal: ");
+            System.out.println("0 - Sair ");
+            System.out.println("Insira a opção desejada: ");
+            opcao = entrada.nextInt();
+
+
+            switch(opcao) {
+
+                case 1:
+                    int quantidadeHoras = 0;
+                    System.out.println("Insira a quantidade de horas para incrementar: ");
+                    quantidadeHoras = entrada.nextInt();
+                    numero1.incrementarHoras( quantidadeHoras ,  numero1);
+                    break;
+                case 2:
+                    double valorNovo = 0;
+                    System.out.println("Insira o novo valor da hora trabalhada: ");
+                    quantidadeHoras = entrada.nextInt();
+                    numero1.alterarValorHora(numero1, quantidadeHoras);
+                    break;
+                case 3:
+                    double salarioFinal = 0;
+                    salarioFinal = numero1.calcularSalario(numero1);
+                    System.out.println("O valor do salário do Funcionário "+numero1.getNome()+" é R$:"+salarioFinal);
+                    break;
+                case 0:
+                    System.out.println("Fim do programa!");
+                    break;
+            }
+        }while(opcao != 0 );
+    }
+}
